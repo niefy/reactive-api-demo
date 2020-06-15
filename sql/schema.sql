ae856da23f4768fa4f928978267955e199f70355
niefy <niefy@qq.com>
2020-06-15 10:11:59 +0800

    配置简化

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE `react-demo` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_unicode_ci';
+USE `react-demo`;
 
 CREATE TABLE customer (id SERIAL PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255));
--- src/main/java/com/github/niefy/repository/CustomerRepository.java
+++ src/main/java/com/github/niefy/repository/CustomerRepository.java
@@ -2,10 +2,10 @@ package com.github.niefy.repository;
 
 import com.github.niefy.entity.Customer;
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import reactor.core.publisher.Flux;
 
-public interface CustomerRepository extends ReactiveCrudRepository<Customer, Long> {
+public interface CustomerRepository extends R2dbcRepository<Customer, Long> {
     @Query("SELECT * FROM customer WHERE last_name = :lastname")
     Flux<Customer> findByLastName(String lastName);
 }
